1a79b09c03b51e2a873bd86132d0872c0bf34da3
eliveltonsf <elivelton.seven@gmail.com>
2020-07-24 21:23:01 -0300

    :art: adjusting-style

--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,25 +1,5 @@
 import styled, { keyframes, css } from 'styled-components';
 
-export const Container = styled.div`
-  max-width: 700px;
-  background: #fff;
-  border-radius: 4px;
-  box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
-  padding: 30px;
-  margin: 80px auto;
-
-  h1 {
-    font-size: 20px;
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-
-    svg {
-      margin-right: 10px;
-    }
-  }
-`;
-
 export const Form = styled.form`
   margin-top: 30px;
   display: flex;
@@ -90,6 +70,12 @@ export const List = styled.ul`
     a {
       color: #7159c1;
       text-decoration: none;
+    }
+
+    button {
+      border: none;
+      background: none;
+      margin-left: 10px;
     }
   }
 `;
